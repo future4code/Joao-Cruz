465547490592e912400b16dcd6d77772a6159c58
JoãoVitordC <sojohhny@gmail.com>
2020-09-28 16:59:08 -0300

    Envio do desafio com JS

---
+++ semana3/aula10/src/index.js
@@ -0,0 +1,43 @@
+a = 10;
+b = 10;
+
+console.log(b);
+// console.log (b) apresentará o valor atual de b = 10.
+b = 5;
+console.log(a, b);
+// console.og(a, b) apresentará os valores atuais de a e b = 10 e 5 respectivamente.
+
+a = 10;
+b = 20;
+c = a;
+b = c;
+a = b;
+console.log(a, b, c)
+// console.og(a,b,c) apresentará os valores atuais de a e b e c = 20, c e 10 respectivamente.
+
+Nome = "Joao";
+Idade = 25;
+typeof Nome // String.
+typeof Idade // Number
+
+prompt ("Que dia e hoje ?");
+console.log("Segunda Feira");
+prompt ("Foi difícil a primeira aula de js ?");
+console.log("Tranquila");
+prompt ("faltam quantas perguntas ?");
+console.log("agora 2");
+prompt ("Vocês sabia que java e uma ilha lindona ?");
+console.log ("Yeap.");
+prompt ("Vai votar em quem 2020 ?");
+console.log ("EU NÃO VOTO BBX's");
+
+let array = ["Sushi", "GrãoDebico", "Churasco", "Pizza", "Sushi2"];
+console.log(array);
+// desafio
+let array = ["GrãoDebico", "Sushi", "Churasco", "Pizza", "Sushi2"];
+console.log(array);
+
+//parte final
+let SuaInternetCaiuHoje = true;
+let VoceVaiEntregarAAtividade = true;
+let MuitoCalorHoje = true;
